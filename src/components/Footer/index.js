88f1e57fe8f47bd34801afed47aa7e7680c1d914
Anurag9497 <anurag.thakur495@gmail.com>
2021-02-24 16:48:22 +0530

    Added Basic Routing

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,13 +6,13 @@ import {Link} from 'react-router-dom';
 function Footer() {
     return (
         <Container className=" bg-dark footer" fluid>
-            <Row className="justify-content-center mt-lg-5" id="logo">
+            <Row className="justify-content-center pt-3" id="logo">
                 GHUMO
             </Row>
             <Row className="justify-content-center mt-1">
                 Copyright &copy; 2021 GHUMO, All Rights Reserved.
             </Row>
-            <Row className="justify-content-center flex-nowrap mt-1">
+            <Row className="justify-content-center flex-nowrap mt-1 pb-3">
                 <Link to="/policy" className="footlink">Privacy Policy</Link>
                 <Link to="/faq" className="footlink">FAQ</Link>
             </Row>
